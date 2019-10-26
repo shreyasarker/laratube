e2266d35dd33f87735d9b6bc753b37ac283dbaa6
shreyas10 <shreya.sarker@gmail.com>
2019-10-26 21:18:26 +0600

    UUID functionality added to unique the user id

--- app/User.php
+++ app/User.php
@@ -5,10 +5,20 @@ namespace LaraTube;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public $incrementing = false;
+
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($model){
+            $model->id = Str::uuid();
+        });
+    }
 
     /**
      * The attributes that are mass assignable.

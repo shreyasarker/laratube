f4166c0ee4cb503c42e886109b35c56a23423d8c
shreyas10 <shreya.sarker@gmail.com>
2019-10-27 23:53:14 +0600

    Update single channel thaumbnail done

--- app/Channel.php
+++ app/Channel.php
@@ -2,9 +2,28 @@
 
 namespace LaraTube;
 
-class Channel extends Model
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\Models\Media;
+
+class Channel extends Model implements HasMedia
 {
+    use HasMediaTrait;
+
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function image(){
+        if($this->media->first()){
+
+            return $this->media()->first()->getFullUrl('thumb');
+        }
+    }
+    public function registerMediaConversions(Media $media = null)
+    {
+        $this->addMediaConversion('thumb')
+            ->width(100)
+            ->height(100);
     }
 }
